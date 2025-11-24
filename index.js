21e2664f401dca4ebc03ed7ca85b57d5f404ef99
Willyjodgreat <joshuachibueze738@gmail.com>
2025-11-24 16:18:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const puppeteer = require('puppeteer-core');
 const chromium = require('chrome-aws-lambda');
 const axios = require('axios');
